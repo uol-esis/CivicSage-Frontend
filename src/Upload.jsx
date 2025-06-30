d161bf42166c77c887ed4d62a453f9282adf19c0
Christian Janßen <christian.janssen1@uni-oldenburg.de>
2025-06-30 13:34:28 +0200

    Adapted basic upload functions to new api

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UploadComponent from './components/UploadComponent';
 import * as CivicSage from 'civic_sage';
 
 export default function Upload() {
   const [inputValue, setInputValue] = useState('');
   const [selectedFile, setSelectedFile] = useState(null);
-  const [selectedFiles, setSelectedFiles] = useState([]);
   const [isValidFile, setIsValidFile] = useState(false);
+  const [fileUID, setFileUID] = useState('');
   const [resetUpload, setResetUpload] = useState(false);
   const [isWebsiteButtonDisabled, setIsWebsiteButtonDisabled] = useState(false);
   const [isUploadButtonDisabled, setIsUploadButtonDisabled] = useState(false);
 
+  /**
+   * Uploads the website to the DB and indexes it
+   */
   const handleConfirmWebsite = () => {
     setIsWebsiteButtonDisabled(true); // Disable the button
     const client = new CivicSage.ApiClient(import.meta.env.VITE_API_ENDPOINT);
     let apiInstance = new CivicSage.DefaultApi(client);
-    let indexWebsiteRequest = new CivicSage.IndexWebsiteRequest(inputValue); // IndexWebsiteRequest | 
+    let indexWebsiteRequest = new CivicSage.IndexWebsiteRequest(inputValue, ""); // IndexWebsiteRequest | 
     apiInstance.indexWebsite(indexWebsiteRequest, (error, data, response) => {
       if (error) {
         console.error(error);
@@ -69,24 +72,53 @@ export default function Upload() {
   };
   */}
 
-  const handleConfirmFile = () => {
+  /**
+   *Uploads a single File and gets the UID from the server. (This later leads to indexing of the file) 
+   */
+  const handleUploadFile = () => {
     setIsUploadButtonDisabled(true); // Disable the upload button
     const client = new CivicSage.ApiClient(import.meta.env.VITE_API_ENDPOINT);
     let apiInstance = new CivicSage.DefaultApi(client);
-    let files = [selectedFile]; // File |
-    apiInstance.indexFiles(files, {}, (error, data, response) => {
+    let file = selectedFile; // File |
+    apiInstance.uploadFile(file, (error, data, response) => {
       if (error) {
         console.error(error);
         alert('Error adding file to the database.');
       } else {
-        console.log('API called successfully.');
-        alert(`SUCCESS! ${selectedFile.name} added to the database.`);
+        console.log('API called successfully. UID: }' + data);
+        setFileUID(data);        
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (fileUID) {
+      indexFile();
+    }
+  }, [fileUID]);
+
+  /**
+   * When the UID is set, tells the server to index the file
+   */
+  const indexFile = () => {
+    const client = new CivicSage.ApiClient(import.meta.env.VITE_API_ENDPOINT);
+    let apiInstance = new CivicSage.DefaultApi(client);
+    let indexFilesRequestInner = [new indexFilesRequestInner(fileUID, selectedFile.name)];
+    apiInstance.indexFiles(indexFilesRequestInner,  (error, data, response) => {
+      if (error) {
+        console.error(error);
+        alert('Error indexing files.');
+      } else {
+        console.log('Files indexed successfully.');
+        alert('Files indexed successfully.');
         setSelectedFile(null); // Clear the selected files after successful submission
         setResetUpload(r => !r); // Toggle reset state to re-render UploadComponent
       }
-      setIsUploadButtonDisabled(false); // Re-enable the upload button
+    setIsUploadButtonDisabled(false); // Re-enable the upload button
     });
-  };
+
+  }
+
 
   return (
   <div className="h-screen">
@@ -125,7 +157,7 @@ export default function Upload() {
         <UploadComponent setFile={setSelectedFile} setValid={setIsValidFile} reset={resetUpload} />  
       </div>
       <button
-        onClick={handleConfirmFile}
+        onClick={handleUploadFile}
         disabled={isUploadButtonDisabled}
         className={`py-2 mt-4 rounded-md text-sm font-semibold text-white shadow-sm ${isUploadButtonDisabled ? 'bg-gray-400' : 'bg-gray-600 hover:bg-indigo-500'}`}
       >
