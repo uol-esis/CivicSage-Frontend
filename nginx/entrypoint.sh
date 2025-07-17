b4671f607f6f992b69a92655b851578a5f75a371
Jonas Pohl <jonas.jp4c@gmail.com>
2025-07-17 14:36:29 +0200

    Fixing docker setup

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 API_HOST="${API_HOST:-http://localhost:8080}"
 
 # Replace the placeholder in the Nginx config template
-envsubst '${API_HOST}${METABASE_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${API_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start Nginx in the foreground
 nginx -g "daemon off;"
