761e0b77235618b8adcfb243e406850bca825bbb
Riphios <christian.janssen1@uni-oldenburg.de>
2025-08-22 23:26:10 +0200

    fixed bug, with filter in overview

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -68,19 +68,37 @@ export default function Overview() {
   }
 
 
-  const filteredFiles = content.files?.filter(
-    file =>
-      (file.fileName && file.fileName.toLowerCase().includes(search.toLowerCase())) ||
-      (file.title && file.title.toLowerCase().includes(search.toLowerCase())) ||
-      (file.uploadDate && file.uploadDate.toLowerCase().includes(search.toLowerCase()))
-  ) || [];
+const filteredFiles = content.files?.filter(
+  file =>
+    (file.fileName && file.fileName.toLowerCase().includes(search.toLowerCase())) ||
+    (file.title && file.title.toLowerCase().includes(search.toLowerCase())) ||
+    (file.uploadDate &&
+      (new Date(file.uploadDate).toLocaleString('de-DE', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit'
+        }).toLowerCase().includes(search.toLowerCase())
+      )
+    )
+) || [];
 
-  const filteredWebsites = content.websites?.filter(
-    website =>
-      (website.title && website.title.toLowerCase().includes(search.toLowerCase())) ||
-      (website.url && website.url.toLowerCase().includes(search.toLowerCase())) ||
-      (website.uploadDate && website.uploadDate.toLowerCase().includes(search.toLowerCase()))
-  ) || [];
+const filteredWebsites = content.websites?.filter(
+  website =>
+    (website.title && website.title.toLowerCase().includes(search.toLowerCase())) ||
+    (website.url && website.url.toLowerCase().includes(search.toLowerCase())) ||
+    (website.uploadDate &&
+      (new Date(website.uploadDate).toLocaleString('de-DE', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit'
+        }).toLowerCase().includes(search.toLowerCase())
+      )
+    )
+) || [];
 
   function showNotification(message, color = 'bg-green-500') {
     setNotification({ message, color });
@@ -143,7 +161,7 @@ export default function Overview() {
               <button
                 className="flex-shrink-0 ml-4 text-white hover:underline"
               >
-                Update
+                Aktualisieren
               </button>
               <button
                 className="flex-shrink-0 ml-4 text-blue-500 hover:underline"
